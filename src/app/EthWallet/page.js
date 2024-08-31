bdd94e6f5cdea047158eb65525ebb85f9cf026d2
Sanket Chakraborty <sanketchak123@gmail.com>
2024-08-31 17:16:26 +0530

    changed button in EthWallet

--- src/app/EthWallet/page.js
+++ src/app/EthWallet/page.js
@@ -56,7 +56,7 @@ const EthWallet = () => {
         onClick={handleAddWallet}
         className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
       >
-        Add Sol Wallet
+        Add Eth Wallet
       </button>
     </div>
     {walletAddresses.length > 0 && (
